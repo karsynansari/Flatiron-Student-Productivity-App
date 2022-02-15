393d17d1d43a56c1c61507d40100dc223f7c33dc
karsyn ansari <karsyn.ansari@gmail.com>
2022-02-15 10:15:26 -0700

    to-do fetch working

--- phase-2-project/src/components/App.js
+++ phase-2-project/src/components/App.js
@@ -1,15 +1,21 @@
-// import { Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import HomePage from "./HomePage";
 import Notepad from "./Notepad";
 import ToDoPage from "./ToDoPage";
 
 function App() {
+  const [toDos, setToDos] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3000/toDo")
+      .then((response) => response.json())
+      .then(setToDos);
+  }, []);
   return (
     <div className="App">
       <Header />
       <HomePage />
-      <ToDoPage />
+      <ToDoPage toDos = {toDos}/>
       <Notepad />
     </div>
   );
