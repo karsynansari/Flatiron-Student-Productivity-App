804619387a21df69f21d39b77c9965a692ba111c
karsyn ansari <karsyn.ansari@gmail.com>
2022-02-15 12:44:25 -0700

    todo items rendering

--- phase-2-project/src/components/ToDoItem.js
+++ phase-2-project/src/components/ToDoItem.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function ToDoItem() {
+function ToDoItem({todo}) {
   return (
-    <div>
-      <p>ToDoItem</p>
+    <div className='"todo'>
+      <div className="todoitem">{todo.task}</div>
+      <div className="duedate">{todo.dueDate}</div>
+      <button className="delete">Delete</button>   
+      <button className="complete">Complete</button>
     </div>
   );
 }
 
-export default ToDoItem
+export default ToDoItem;
--- phase-2-project/src/components/ToDoPage.js
+++ phase-2-project/src/components/ToDoPage.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import ToDoForm from "./ToDoForm"
-import ToDoItem from './ToDoItem';
+import React from "react";
+import ToDoForm from "./ToDoForm";
+import ToDoItem from "./ToDoItem";
 
-function ToDoPage() {
+function ToDoPage({ toDos }) {
+  const eachToDo = toDos.map((todo) => <ToDoItem key={todo.id} todo={todo} />);
   return (
     <div>
       <div>ToDoPage</div>
       <ToDoForm />
-      <ToDoItem />
+      <ul> {eachToDo}</ul>
     </div>
   );
 }
 
-export default ToDoPage
+export default ToDoPage;
